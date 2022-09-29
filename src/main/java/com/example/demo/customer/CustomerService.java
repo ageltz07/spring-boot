6790471bd67736c934b3ced406fe0e5a149f60bd
ageltz07 <ageltz07@gmail.com>
2022-09-29 16:14:26 -0400

    API with Spring MVC

--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,8 +1,8 @@
 package com.example.demo.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -17,7 +17,15 @@ public class CustomerService {
         this.customerRepo = customerRepo;
     }
 
-    List<Customer> getCustomer() {
+    List<Customer> getCustomers() {
         return customerRepo.getCustomers();
+    }
+
+    Customer getCustomer(Long id) {
+        return getCustomers()
+                .stream()
+                .filter(customer -> customer.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("customer not found"));
     }
 }
