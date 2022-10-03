35dba0f9475f49c62038bd6c06ff8228c41bd1a5
ageltz07 <ageltz07@gmail.com>
2022-10-03 13:52:09 -0400

    Spring Data and Databases

--- src/main/java/com/example/demo/customer/Customer.java
+++ src/main/java/com/example/demo/customer/Customer.java
@@ -3,20 +3,26 @@ package com.example.demo.customer;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
+@Entity
+@Table
 public class Customer {
-    private final Long id;
+    @Id
+    private Long id;
     @NotBlank(message = "name must be not empty")
-    private final String name;
+    private String name;
     @NotBlank(message = "password must be not empty")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    private final String password;
+    private String password;
 
     @NotBlank(message = "email must be not empty")
     @Email
-    private final String email;
+    private String email;
 
     Customer(Long id,
              String name,
@@ -26,6 +32,10 @@ public class Customer {
         this.name = name;
         this.password = password;
         this.email = email;
+    }
+
+    public Customer() {
+
     }
 
     @JsonProperty("customerId")
--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -3,30 +3,27 @@ package com.example.demo.customer;
 import com.example.demo.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @Component
 public class CustomerService {
 
-    private final CustomerRepo customerRepo;
+    private final CustomerRepository customerRepository;
 
     @Autowired
     public CustomerService(
-            CustomerRepo customerRepo) {
-        this.customerRepo = customerRepo;
+            CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
     }
 
     List<Customer> getCustomers() {
-        return customerRepo.getCustomers();
+        return customerRepository.findAll();
     }
 
     Customer getCustomer(Long id) {
-        return getCustomers()
-                .stream()
-                .filter(customer -> customer.getId().equals(id))
-                .findFirst()
+        return customerRepository
+                .findById(id)
                 .orElseThrow(
                         () -> new NotFoundException(
                                 "customer with id " + id + " not found"));
