2ecbb78381329dc14d4ff058e3a6651adddc245e
ageltz07 <ageltz07@gmail.com>
2022-10-07 11:04:51 -0400

    Logging

--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,13 +1,19 @@
 package com.example.demo.customer;
 
 import com.example.demo.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Component
+@Service
 public class CustomerService {
+
+    private final static Logger LOGGER =
+            LoggerFactory.getLogger(CustomerService.class);
 
     private final CustomerRepository customerRepository;
 
@@ -18,6 +24,7 @@ public class CustomerService {
     }
 
     List<Customer> getCustomers() {
+        LOGGER.info("getCustomers was called");
         return customerRepository.findAll();
     }
 
@@ -25,7 +32,11 @@ public class CustomerService {
         return customerRepository
                 .findById(id)
                 .orElseThrow(
-                        () -> new NotFoundException(
-                                "customer with id " + id + " not found"));
+                        () -> {
+                            NotFoundException notFoundException = new NotFoundException(
+                                    "customer with id " + id + " not found");
+                            LOGGER.error("error in getting customer {}", id, notFoundException);
+                            return notFoundException;
+                        });
     }
 }
